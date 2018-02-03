dbcf8d0bd19029ab9a00ab398da0fb484f20f0a8
Joshua Simmons <joshua.simmons@emptypath.com>
2018-02-02 21:33:52 -0700

    actually produce help message on --help

--- main.cpp
+++ main.cpp
@@ -13,10 +13,15 @@ int main(int argc, char * argv[]) {
   try {
     po::options_description opts_desc("Options");
     opts_desc.add_options()
-      ("help", "produce this help message");
+      ("help", "produce this helpful message");
     po::variables_map cmd_opts;
     po::store(po::parse_command_line(argc, argv, opts_desc), cmd_opts);
     po::notify(cmd_opts);
+
+    if (cmd_opts.count("help")) {
+      cout << opts_desc << endl;
+      return 0;
+    }
 
   } catch (...) {
     cerr << "Error of unknown type!" << endl;
